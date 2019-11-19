c0f1a3997c1ee4d4811f41093bd4881d3ebed1c1
kouinkouin <me@kouinkouin.com>
2019-11-19 09:55:48 +0100

    feat: mkdir www-data home dir

--- files/run.sh
+++ files/run.sh
@@ -3,5 +3,7 @@ set -e
 
 [ $(id -u www-data) -ne $UID -o $(id -g www-data) -ne $GID ] && groupmod -g $GID www-data && usermod -u $UID www-data
 
+mkdir -p /var/www && chown -R www-data. /var/www
+
 exec sudo -u www-data $@
 
