22e937c32f08d00b86de7598d9017da37f9c1dca
kouinkouin <kouinkouin@users.noreply.github.com>
2020-08-04 20:50:51 +0000

    fix: set permissions on /var/www only if the folder did not exist

--- files/run.sh
+++ files/run.sh
@@ -3,7 +3,8 @@ set -e
 
 [ $(id -u www-data) -ne $UID -o $(id -g www-data) -ne $GID ] && groupmod -g $GID www-data && usermod -u $UID www-data
 
-mkdir -p /var/www && chown -R www-data. /var/www
+if [ ! -d /var/www ]; then
+  mkdir -p /var/www && chown -R www-data. /var/www
+fi
 
 exec sudo -E -u www-data $@
-
